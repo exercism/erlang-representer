6bd6d84fe708e0fac503960beade9197dc4de2a6
Norbert Melzer <timmelzer@gmail.com>
2020-08-28 17:32:50 +0200

    print version

--- src/erl_representer.erl
+++ src/erl_representer.erl
@@ -3,13 +3,19 @@
 %% API exports
 -export([main/1]).
 
+-define(APP, ?MODULE).
+
 %%====================================================================
 %% API functions
 %%====================================================================
 
 %% escript Entry point
 main(Args) ->
+    ok = application:load(?APP), %% We need to load the application to its keys
+                                 %% available later on.
     io:format("Args: ~p~n", [Args]),
+    {ok, Version} = application:get_key(?APP, vsn),
+    io:format("Version: ~s~n", [Version]),
     erlang:halt(0).
 
 %%====================================================================
