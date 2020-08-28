724174b8f70693440d5694815bd955ddb2d6d86b
Norbert Melzer <timmelzer@gmail.com>
2020-08-28 18:59:05 +0200

    bootstrap the logger

--- src/erl_representer.erl
+++ src/erl_representer.erl
@@ -5,16 +5,21 @@
 
 -define(APP, ?MODULE).
 
+-include_lib("kernel/include/logger.hrl").
+
 %%====================================================================
 %% API functions
 %%====================================================================
 
 %% escript Entry point
-main(_Args) ->
+main([]) ->
     ok = application:load(?APP), %% We need to load the application to its keys
                                  %% available later on.
+    set_up_logger(2),
+    ?LOG_ERROR(version()),
     Version = version(),
     io:format("Version: ~s~n", [Version]),
+    timer:sleep(6000),
     erlang:halt(0).
 
 %%====================================================================
@@ -40,3 +45,13 @@ format_date(Date0) ->
     {Hour, Time1} = lists:split(2, Time0),
     {Minute, Seconds} = lists:split(2, Time1),
     lists:flatten(io_lib:format("~s-~s-~sT~s:~s:~sZ", [Year, Month, Day, Hour, Minute, Seconds])).
+
+set_up_logger(Verbosity) ->
+    Level = case Verbosity of
+                0 -> notice;
+                1 -> info;
+                X when is_integer(X), X >= 2 -> debug
+            end,
+    logger:add_handler(file_logger, logger_std_h, #{config => #{file => "foo.log"}}),
+    logger:update_primary_config(#{level => Level}),
+    io:format("~p~n", [logger:get_primary_config()]).
