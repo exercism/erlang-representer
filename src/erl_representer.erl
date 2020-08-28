498cf203d390429451f06e82ce80fd59ba82468e
Norbert Melzer <timmelzer@gmail.com>
2020-08-28 17:37:06 +0200

    do not print arguments

--- src/erl_representer.erl
+++ src/erl_representer.erl
@@ -10,10 +10,9 @@
 %%====================================================================
 
 %% escript Entry point
-main(Args) ->
+main(_Args) ->
     ok = application:load(?APP), %% We need to load the application to its keys
                                  %% available later on.
-    io:format("Args: ~p~n", [Args]),
     {ok, Version} = application:get_key(?APP, vsn),
     io:format("Version: ~s~n", [Version]),
     erlang:halt(0).
