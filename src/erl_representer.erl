828579d34bbee2eedc61a5e81749ae3c7d2c20c4
Norbert Melzer <timmelzer@gmail.com>
2020-08-28 17:53:25 +0200

    nicer formatting of version

--- src/erl_representer.erl
+++ src/erl_representer.erl
@@ -13,10 +13,30 @@
 main(_Args) ->
     ok = application:load(?APP), %% We need to load the application to its keys
                                  %% available later on.
-    {ok, Version} = application:get_key(?APP, vsn),
+    Version = version(),
     io:format("Version: ~s~n", [Version]),
     erlang:halt(0).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+version() ->
+    {ok, Version0} = application:get_key(?APP, vsn),
+    Version1 = string:split(Version0, "-"),
+    case Version1 of
+        [Date, "dirty"] ->
+            format_date(Date) ++ "-dirty-commit";
+        [Date, Commit] ->
+            format_date(Date) ++ "-" ++ Commit;
+        ["dev"] ->
+            "inofficial-dev-build"
+    end.
+
+format_date(Date0) ->
+    {Year, Date1} = lists:split(4, Date0),
+    {Month, Date2} = lists:split(2, Date1),
+    {Day, Time0} = lists:split(2, Date2),
+    {Hour, Time1} = lists:split(2, Time0),
+    {Minute, Seconds} = lists:split(2, Time1),
+    lists:flatten(io_lib:format("~s-~s-~sT~s:~s:~sZ", [Year, Month, Day, Hour, Minute, Seconds])).
